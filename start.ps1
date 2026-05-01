f21f2b1e7d02abf2e7bbdebe80eeb53061d23650
SimonDdungu <dev.simonddungu@gmail.com>
2026-05-01 21:01:45 +0300

    Updated Scripts

--- start.ps1
+++ start.ps1
@@ -1,7 +1,24 @@
 $ErrorActionPreference = "Stop"
 
 Write-Host "Installing dependencies..."
-pnpm install
+Write-Host "Removing node_modules if exists..."
+if (Test-Path "node_modules") {
+    Remove-Item -Recurse -Force "node_modules"
+}
+pnpm install --frozen-lockfile
+
+Write-Host ""
+
+Write-Host "Checking for .env file..."
+if (!(Test-Path ".env")) {
+    Write-Host ">>> Creating .env file using secrets from .env.example..."
+    Copy-Item ".env.example" ".env"
+} else {
+    Write-Host ".env already exists!"
+}
+
+Write-Host ""
+Write-Host ">>>>> Don't forget to update .env file to match your credentials! <<<<<"
 Write-Host ""
 
 Write-Host "Starting Frontend Server..."
--- start.sh
+++ start.sh
@@ -2,7 +2,21 @@
 set -e
 
 echo "Installing dependencies..."
-pnpm install
+rm -rf node_modules
+pnpm install --frozen-lockfile
+echo ""
+
+echo "Checking for .env file..."
+echo "Removing node_modules if exists..."
+if [ ! -f ".env" ]; then
+    echo ">>> Creating .env file using secrets from .env.example..."
+    cp .env.example .env
+else
+    echo ".env file already exists!"
+fi
+
+echo ""
+echo ">>>>> Don't forget to update .env file to match your credentials! <<<<<"
 echo ""
 
 echo "Starting Frontend Server..."
